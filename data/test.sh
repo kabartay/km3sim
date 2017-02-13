29367046eb57c52b1683b1f8c5af0fb494306b5a
Moritz Lotze <mlotze@km3net.de>
2017-02-13 18:15:57 +0100

    det files

--- data/test.sh
+++ data/test.sh
@@ -5,5 +5,5 @@ rm -f orca.gdml
 km3sim \
   -i gseagen.muon-CC.3-100GeV-9.1E7-1bin-3.0gspec.ORCA115_9m_2016.1.evt \
   -o gseagen.muon-CC.3-100GeV-9.1E7-1bin-3.0gspec.ORCA115_9m_2016.1.out.evt \
-  -d orca_120strings_av23min20mhorizontal_18OMs_alt9mvertical_v1.detx \
+  -d orca.detx \
   -p INPUTParametersRun_3500_scatter_WPD3.3_p0.0075_1.0
